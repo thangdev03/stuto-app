1283c25dae7e4f7c4829327d7248b7128f4ae878
thangdev03 <nhatthangdev@gmail.com>
2024-01-10 19:07:52 +0700

    create study room

--- src/pages/client/StudyRoom.jsx
+++ src/pages/client/StudyRoom.jsx
@@ -1,6 +1,38 @@
+import FriendsList from "../../components/FriendsList"
+import { FaPlus } from "react-icons/fa";
+
+
 function StudyRoom() {
     return (
-        <h1>StudyRoom Page</h1>
+        <div className="ml-80 mr-[416px] mt-10">
+            <button className="w-full py-3 bg-primaryColor rounded-3xl text-white font-semibold flex items-center justify-center gap-2 text-xl shadow-blockShadow hover:brightness-105">
+                <FaPlus className="text-base"/>
+                Tạo phòng mới
+            </button>
+
+            {/* User's Rooms */}
+            <div className="mt-10">
+                <h2 className="font-semibold text-2xl">Phòng riêng</h2>
+                <div className="mt-4 grid grid-cols-4 gap-y-2">
+                    <div className="w-48 h-72 bg-[#B196FF]/70 rounded-xl">
+                        <div>
+                            <h3>Vượt qua vật lý đại ĐC ^.^</h3>
+                            <p>2 Stutoers đang học</p>
+                            <p>Mic cho phép</p>
+                            <p>Camera cho phép</p>
+                            <p>Vật lý đại cương</p>
+                        </div>
+                        <button>
+                            Tham gia phòng
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            {/* Public Rooms */}
+
+            <FriendsList />
+        </div>
     )
 }
 
