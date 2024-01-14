c944bd7a231b62bac7e9f67b79ee72bc4d40f6ff
thangdev03 <nhatthangdev@gmail.com>
2024-01-14 10:08:26 +0700

    add search in friend list

--- src/components/FriendsList.jsx
+++ src/components/FriendsList.jsx
@@ -1,51 +1,70 @@
+import { useState } from "react";
 import { IoMdSearch  } from "react-icons/io";
 
 function FriendsList() {
+    const friendsList = [
+        {
+            id: 1,
+            name: 'Chu Bin',
+            school: 'NEU',
+            major: 'Kinh tế tài nguyên và môi trường',
+            avatar: ''
+        },
+        {
+            id: 2,
+            name: 'Phương Ly',
+            school: 'NEU',
+            major: 'Công nghệ thông tin',
+            avatar: ''
+        },
+        {
+            id: 3,
+            name: 'Andree Trái Tay',
+            school: 'NEU',
+            major: 'Công nghệ tài chính',
+            avatar: ''
+        }
+    ];
+
+    const [inputValue, setInputValue] = useState("");
 
     return (
         <div className="fixed top-28 right-7 h-[500px] w-[360px] bg-boxBackground rounded-3xl py-7 pl-7 pr-4 flex flex-col">
             <h3 className="font-semibold text-lg text-textColor mb-2">StuToers đã kết nối</h3>
-            <form action="#" method="post" className="bg-[#dfdfdf79] rounded-full flex opacity-60 focus-within:opacity-100">
+            <div className={`bg-[#dfdfdf79] rounded-full flex focus-within:opacity-100 transition-all ${inputValue? "opacity-100": "opacity-60"}`}>
                 <input 
                     type="text"
                     placeholder="Nhập thông tin của bạn cần tìm..."
-                    className="w-full bg-transparent py-2 px-4 focus:outline-none text-textColor text-sm "
+                    className="w-full bg-transparent py-2 px-4 focus:outline-none text-textColor text-sm"
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value.toLowerCase())}
                 />
                 <div className="p-2 text-[#9C9C9C] hover:text-[#333333]">
                     <IoMdSearch id="searchBtn" className="cursor-pointer group-focus-within:text-textColor text-2xl"/>
                 </div>
-            </form>
-            {/* Friends list */}
-            <div className="mt-5 flex flex-col gap-1 grow overflow-y-auto">
-                <a href="#" className="flex items-top hover:cursor-pointer hover:bg-gray-100 p-2 rounded-lg">
-                    <div className="relative mr-4">
-                        <img 
-                            src="./img/default-avatar.png" 
-                            alt=""
-                            className="w-10 h-10 rounded-full object-cover" 
-                        />
-                        <div className={`absolute top-7 left-7 bg-green-500 w-3 h-3 rounded-full`}></div>
-                    </div>
-                    <div className="w-[calc(100%-58px)]">
-                        <h4 className="font-semibold">Chu Bin</h4>
-                        <p className="text-xs w-full truncate">NEU, Natural Resources Economics Resources Economic Resources Economic</p>
-                    </div>
-                </a>
-                <a href="#" className="flex items-top hover:cursor-pointer hover:bg-gray-100 p-2 rounded-lg">
-                    <div className="relative mr-4">
-                        <img 
-                            src="./img/default-avatar.png" 
-                            alt=""
-                            className="w-10 h-10 rounded-full object-cover" 
-                        />
-                        <div className={`absolute top-7 left-7 bg-green-500 w-3 h-3 rounded-full`}></div>
-                    </div>
-                    <div className="w-[calc(100%-56px)] overflow-hidden">
-                        <h4 className="font-semibold">Chu Bin</h4>
-                        <p className="text-xs w-full truncate">NEU, Natural Resources Economics</p>
-                    </div>
-                </a>
             </div>
+            {/* Friends list */}
+            <ul className="mt-5 flex flex-col gap-1 grow overflow-y-auto">
+                {friendsList.map((friend) => friend.name.toLowerCase().includes(inputValue) || friend.major.toLowerCase().includes(inputValue) ? (
+                    <li className="flex items-top hover:cursor-pointer hover:bg-gray-100 p-2 rounded-lg cursor-pointer">
+                        <div className="relative mr-4">
+                            <img 
+                                src="./img/default-avatar.png" 
+                                alt=""
+                                className="w-10 h-10 rounded-full object-cover" 
+                            />
+                            <div className={`absolute top-7 left-7 bg-green-500 w-3 h-3 rounded-full`}></div>
+                        </div>
+                        <div className="w-[calc(100%-58px)]">
+                            <h4 className="font-semibold">{friend.name}</h4>
+                            <p className="text-xs w-full truncate">{friend.school}, {friend.major}</p>
+                        </div>
+                    </li>
+                ) : (
+                    null
+                )
+                )}
+            </ul>
         </div>
     );
 }
